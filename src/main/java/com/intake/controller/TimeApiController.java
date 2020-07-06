e8bad01a2b02765088b514373d9f86dd184ffd76
RajalakshmiElumalaiCts <laksh@192.168.1.143>
2020-07-06 14:41:16 -0500

    JWT fix

--- src/main/java/com/intake/controller/TimeApiController.java
+++ src/main/java/com/intake/controller/TimeApiController.java
@@ -4,8 +4,10 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Set;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,25 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/time_api")
 public class TimeApiController {
 	
-		@RequestMapping("/local_time")
-		public String getLocalTime() {
+		@RequestMapping(value = "/local_time", method = RequestMethod.GET)
+		public ResponseEntity<String>  getLocalTime() {
 			LocalDate localDate = LocalDate.now();
 			System.out.println("currentTime 1------->"+localDate.toString());
-			return localDate.toString();	
+			return new ResponseEntity<String>(localDate.toString(), HttpStatus.OK);
 			
 		}
 		
-		@GetMapping(value="/zone_time")
-		public String getZoneTime(@RequestParam("zone_id") final String zoneId) {
+		@RequestMapping(value="/zone_time", method = RequestMethod.GET)
+		public ResponseEntity<String> getZoneTime(@RequestParam("zone_id") final String zoneId) {
 			System.out.println("zoneId ---------------->"+zoneId);
 			ZoneId zone = ZoneId.of(zoneId);
 			String zoneTime = LocalDate.now(zone).toString();
-			return zoneTime;
+			return new ResponseEntity<String>(zoneTime, HttpStatus.OK);
 		}
 		
-		@GetMapping(value="/zone_ids")
-		public Set<String> getZoneIds(){
-			return ZoneId.getAvailableZoneIds();		
+		@RequestMapping(value="/zone_ids", method = RequestMethod.GET)
+		public ResponseEntity<Set<String>> getZoneIds(){
+			return new ResponseEntity<Set<String>>(ZoneId.getAvailableZoneIds(), HttpStatus.OK);		
 		}
 
 
